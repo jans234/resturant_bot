ac3f7c8c073417b8519160c42e7cc03602b79804
muhammad_afaq <muhammadafaq8950@gmail.com>
2024-09-27 23:00:34 +0500

    sqlite update

--- interface.py
+++ interface.py
@@ -1,3 +1,8 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import streamlit as st
 from streamlit_chat import message as st_message
 from resturant_chatbot import *
